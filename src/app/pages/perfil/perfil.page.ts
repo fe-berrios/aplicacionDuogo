a3ba38ea3da72f2a8860752bb1f82f2af7a31258
fe-berrios <fe.berrios.ov@gmail.com>
2024-10-21 18:57:47 -0300

    Visualización ruta de viaje en lista

--- src/app/pages/perfil/perfil.page.ts
+++ src/app/pages/perfil/perfil.page.ts
@@ -8,73 +8,15 @@ import { UsuarioService } from 'src/app/services/usuario.service';
   styleUrls: ['./perfil.page.scss'],
 })
 export class PerfilPage implements OnInit {
+  usuario: any; // Declaramos la propiedad 'usuario'
 
-  usuarioForm: FormGroup = new FormGroup({
-    rut: new FormControl({ value: '', disabled: true }),
-    nombre: new FormControl({ value: '', disabled: true }),
-    apellido: new FormControl({ value: '', disabled: true }),
-    correo: new FormControl({ value: '', disabled: true }),
-    telefono: new FormControl('', [Validators.required, Validators.pattern("[0-9]{9}")]),
-    tipo_usuario: new FormControl('', [Validators.required]),
-    nombre_auto: new FormControl('', Validators.minLength(3)),
-    capacidad_auto: new FormControl('', Validators.min(2)),
-  });
-
-  constructor(private usuarioService: UsuarioService) {}
+  constructor() { }
 
   ngOnInit() {
-    const usuarioSesion = sessionStorage.getItem('usuario');
+    const usuarioSesion = localStorage.getItem('usuario');
+
     if (usuarioSesion) {
-      const usuarioData = JSON.parse(usuarioSesion);
-      this.usuarioForm = new FormGroup({
-        rut: new FormControl({ value: usuarioData.rut, disabled: true }),
-        nombre: new FormControl({ value: usuarioData.nombre, disabled: true }),
-        apellido: new FormControl({ value: usuarioData.apellido, disabled: true }),
-        correo: new FormControl({ value: usuarioData.correo, disabled: true }),
-        telefono: new FormControl(usuarioData.telefono, [Validators.required, Validators.pattern("[0-9]{9}")]),
-        tipo_usuario: new FormControl(usuarioData.tipo_usuario, [Validators.required]),
-        nombre_auto: new FormControl(usuarioData.nombre_auto, Validators.minLength(3)),
-        capacidad_auto: new FormControl(usuarioData.capacidad_auto, Validators.min(2)),
-      });
-
-      this.checkIfConductor();  // Activar o desactivar los campos de auto
+      this.usuario = JSON.parse(usuarioSesion); // Guardamos los datos en la propiedad 'usuario'
     }
   }
-
-  // Si el tipo de usuario es 'estudiante_conductor', se activan los campos de auto
-  checkIfConductor() {
-    const tipoUsuario = this.usuarioForm.get('tipo_usuario')?.value;
-    
-    if (tipoUsuario === 'estudiante_conductor') {
-      this.usuarioForm.get('nombre_auto')?.enable();
-      this.usuarioForm.get('capacidad_auto')?.enable();
-    } else {
-      this.usuarioForm.get('nombre_auto')?.disable();
-      this.usuarioForm.get('capacidad_auto')?.disable();
-    }
-  }
-  
-
-  // Llamado al cambiar el tipo de usuario
-  onTipoUsuarioChange() {
-    this.checkIfConductor();
-  }
-
-  // Actualizar usuario
-  async updateUsuario() {
-    const rut_buscar = this.usuarioForm.controls['rut'].value || "";
-
-    // Verifica el tipo de usuario y resetea los campos de auto si es estudiante
-    if (this.usuarioForm.controls['tipo_usuario'].value === 'estudiante') {
-      this.usuarioForm.controls['nombre_auto'].setValue('');
-      this.usuarioForm.controls['capacidad_auto'].setValue('');
-    }
-
-    if (await this.usuarioService.updateUsuario(rut_buscar, this.usuarioForm.value)) {
-      alert("Usuario modificado con éxito!");
-      // Aquí podrías actualizar la sesión, si lo deseas
-    } else {
-      alert("ERROR! Usuario no modificado.");
-    }
-  }  
 }
--- src/app/pages/viaje/viaje.page.ts
+++ src/app/pages/viaje/viaje.page.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { ViajeService } from 'src/app/services/viaje.service';
 
+// Import para leaflet
+import * as leaflet from 'leaflet';
+import * as geo from 'leaflet-control-geocoder';
+import 'leaflet-routing-machine';
+import { JsonPipe } from '@angular/common';
+
 @Component({
   selector: 'app-viaje',
   templateUrl: './viaje.page.html',
@@ -10,16 +16,88 @@ export class ViajePage implements OnInit {
 
   viajes: any[] = [];
 
+  // Leaflet (mapa)
+  private map: leaflet.Map | undefined;
+  private geocoder: geo.Geocoder | undefined;
+
+  // Variables que rescatan informacion del mapa
+  latitud: number = 0;
+  longitud: number = 0;
+  direccion: string = "";
+  distanciaMetros: number = 0;
+  tiempoSegundos: number = 0;
+
   constructor(private viajeService: ViajeService) { }
 
   ngOnInit() {
+    this.initMapa();
     this.getViajes();
   }
 
-  agendarViaje(){
-    alert("Agendado!")
+  initMapa(){
+    // 'locate' Ubicación actual utiliza TÚ ubicación de dispositivo.
+    this.map = leaflet.map('map_lista').setView([-33.59838016321339, -70.57879780298838], 16);
+    leaflet.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(this.map);
+
+    // Agregar buscador de direcciones en el mapa
+    this.geocoder = geo.geocoder({
+      placeholder: "Ingrese dirección a buscar",
+      errorMessage: "Dirección NO encontrada"
+    }).addTo(this.map);
+
+    this.geocoder.on('markgeocode', (e) => {
+      // Asignar latitud, longitud y dirección desde la búsqueda en el mapa
+      this.latitud = e.geocode.properties['lat'];
+      this.longitud = e.geocode.properties['lon'];
+      this.direccion = e.geocode.properties['display_name'];
+
+      if (this.map) {
+        leaflet.Routing.control({
+          waypoints: [
+            leaflet.latLng(-33.59838016321339, -70.57879780298838),
+            leaflet.latLng(this.latitud, this.longitud)
+          ],
+          fitSelectedRoutes: true
+        }).on('routesfound', (e) => {
+          // Obtener distancia y tiempo de la ruta
+          this.distanciaMetros = e.routes[0].summary.totalDistance;
+          this.tiempoSegundos = e.routes[0].summary.totalTime;
+
+        }).addTo(this.map);
+      }
+    });
   }
 
+  verRuta(viaje: any) {
+    // Coordenadas del viaje
+    const origen = leaflet.latLng(-33.59838016321339, -70.57879780298838); // Coordenadas de Puente Alto
+    const destino = leaflet.latLng(viaje.latitud, viaje.longitud); // Coordenadas del destino del viaje
+  
+    if (this.map) {
+      // Limpiar rutas anteriores
+      this.map.eachLayer((layer) => {
+        if (layer instanceof leaflet.Routing.Control) {
+          this.map?.removeLayer(layer);
+        }
+      });
+  
+      // Crear nueva ruta en el mapa
+      leaflet.Routing.control({
+        waypoints: [origen, destino],
+        fitSelectedRoutes: true
+      }).addTo(this.map);
+    }
+  }
+  
+  unirme(viaje: any) {
+    // Acción para el botón 'Unirme' (Aún sin implementar)
+    console.log('Unirse al viaje', viaje);
+  }
+
+  // Obtener viajes desde Storage
   async getViajes(){
     this.viajes = await this.viajeService.getViajes(); 
   }
